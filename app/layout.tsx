4f1883a9d998f0d316f28d0bea48b58cf9387422
Hilmi Zikri <zikrihilmi15@gmail.com>
2026-05-15 22:19:09 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ import { Toaster } from 'react-hot-toast'
 const inter = Inter({ subsets: ['latin'] })
 
 const siteUrl = 'https://www.eisd.site/'
-const title = 'Lab EISD - Enterprise Intelligence System Development Laboratory'
+const title = 'EISD Laboratory - Enterprise Intelligence System Development Laboratory'
 const description = 'Laboratory focused on software development, IoT, AI, and digital innovation'
 
 export const metadata: Metadata = {
