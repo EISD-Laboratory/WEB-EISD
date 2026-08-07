0db63f5a4ce5745129124d988e3cc48cfe345532
Hilmi Zikri <zikrihilmi15@gmail.com>
2026-08-07 10:44:07 +0700

    Harden CSP/permissions headers and fix RSS entity-decode order

--- lib/articles.ts
+++ lib/articles.ts
@@ -67,12 +67,12 @@ const getCdataTagValue = (xml: string, tag: string) => unwrapCdata(getXmlTagValu
 
 const decodeHtml = (value: string) =>
   value
-    .replace(/&amp;/g, '&')
     .replace(/&quot;/g, '"')
     .replace(/&#39;/g, "'")
     .replace(/&lt;/g, '<')
     .replace(/&gt;/g, '>')
     .replace(/&nbsp;/g, ' ')
+    .replace(/&amp;/g, '&')
 
 const formatArticleDate = (value: string) =>
   new Intl.DateTimeFormat('en-US', {
